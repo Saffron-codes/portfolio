e9a38cd7c9bf9641f5d833bf14fc086c9ec4ddbf
Saffron-codes <saffrontdionysius@gmail.com>
2023-08-06 21:18:51 +0530

    removed web projects from list

--- src/data/portfolio.jsx
+++ src/data/portfolio.jsx
@@ -15,35 +15,8 @@ export default [
    },
    {
       title:'Note Warden',
-      imgUrl:'',
+      imgUrl:'https://raw.githubusercontent.com/Saffron-codes/note_warden/dev/assets/Note%20Warden.png',
       stack:['Flutter','Android'],
       link:'https://github.com/saffron-codes/note_warden'
    },
-   {
-      title: 'Todo List',
-      imgUrl: 'assets/todolist-flask.png',
-      stack: ['Python', 'Flask', 'HTML'],
-      link: 'https://github.com/Saffron-codes/todolist_flask',
-   },
-   {
-      title: 'Link in Bio',
-      imgUrl: 'assets/sd-links.png',
-      stack: ['HTML', 'CSS', 'JS'],
-      link: 'https://links-saffron-dionysius.netlify.app/',
-   },
-   {
-      title: 'Robotics Club Website',
-      imgUrl: 'assets/sjcrc.png',
-      stack: ['HTML', 'CSS', 'JS'],
-      link: 'https://sjcrc.netlify.app',
-   },
-   {
-      title: 'Developer Portfolio',
-      imgUrl: '',
-      stack: ['React', 'Tailwind', 'Vite'],
-      link: 'https://github.com/saffron-codes/portfolio',
-   },
-
-
-
 ]
